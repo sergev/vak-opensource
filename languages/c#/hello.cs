6de866d11289c4f28b7485e6fa5903f9b03516dc
Serge <serge@vak.ru>
2017-02-26 16:44:45 -0800

    Add more hello-world examples.

---
+++ languages/c#/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    class Entry
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
