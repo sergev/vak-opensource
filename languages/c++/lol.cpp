3f2aafa7d8cac1b65de8fce4469bf66c76512648
Serge Vakulenko <serge.vakulenko@gmail.com>
2017-05-12 17:59:31 -0700

    Add lol.cpp.

---
+++ languages/c++/lol.cpp
@@ -0,0 +1,13 @@
+// clang++ -std=c++11 lol.cc -o lol
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int lol = 10;
+    int l​ol = 10;
+    int l​o​l = 10;
+    int l​​ol = lol + l​ol + l​o​l;
+    cout << l​​ol << endl;
+    return 0;
+}
