6f0cdb051d4fed40460e07da4643b8f22165bb7a
Serge Vakulenko <serge.vakulenko@gmail.com>
2017-10-09 01:10:11 -0700

    Finish Wi-Fi activity demo for OLED board.

--- microcontrollers/arduino-esp8266/oled-life/oled-life.ino
+++ microcontrollers/arduino-esp8266/oled-life/oled-life.ino
@@ -1,5 +1,22 @@
 /*
  * Convey's Life on 128x32 display.
+ *
+ * Copyright (C) 2017 Serge Vakulenko, <vak@besm6.org>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  */
 #include <SPI.h>
 #include <Wire.h>
--- microcontrollers/arduino-esp8266/oled-scan/oled-scan.ino
+++ microcontrollers/arduino-esp8266/oled-scan/oled-scan.ino
@@ -1,5 +1,22 @@
 /*
- * Convey's Life on 128x32 display.
+ * Show the Wi-Fi band activity.
+ *
+ * Copyright (C) 2017 Serge Vakulenko, <vak@besm6.org>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  */
 #include <SPI.h>
 #include <Wire.h>
@@ -15,7 +32,9 @@ Adafruit_SSD1306 display(D0);
 #define MX      128     /* x-side length */
 #define MY      32      /* y-side length */
 
-static int bar[MX];
+#define NB      (11+4)  /* 11 channels plus 2+2 side bands */
+
+static int band[NB];
 
 void setup()
 {
@@ -42,56 +61,92 @@ void setup()
 //
 // Reset the bar.
 //
-static void reset_bar()
+static void reset_bands()
 {
-    int x;
-    for (x=0; x<MX; x++) {
-        bar[x] = 0;
+    int i;
+    for (i=0; i<NB; i++) {
+        band[i] = -99;
     }
 }
 
 //
 // Increase the bar.
 //
-static void raise_bar(int x0, int dy)
+static void draw_band(int index)
 {
-    int x, y;
-    for (x = x0; x < x0 + 7; x++) {
-        bar[x] += dy;
-        y = MY - bar[x];
-        if (y >= 0 && y < MY)
-            display.drawPixel(x, y, WHITE);
+    int x = index * 8;
+    int y;
+    int level = band[index] + 95;
+
+    for (y = MY-1; y >= 0; y-=2) {
+        if (level < 0)
+            break;
+        display.drawFastHLine(x, y, 7, WHITE);
+        level -= 4;
+    }
+}
+
+void draw_bands()
+{
+    int i;
+    for (i=0; i<NB; i++) {
+          draw_band(i);
     }
 }
 
 //
-// Draw the signal level at given channel
+// Add a signal level to the band.
+//
+void add_band(int index, int q)
+{
+    int p = band[index];
+
+    if (p < q) {
+        int t = p;
+        p = q;
+        q = t;
+    }
+    // P and Q are signal levels in dB, P >= Q.
+    // Compute the dB value of the sum of two power levels.
+    if (p == q) {
+        // When P and Q are equal, result is P+3.01 dB
+        p += 3;
+    }
+    else if (p-1 == q) {
+        // When P and Q differ by 1dB, result is P+1.76 dB
+        p += 2;
+    }
+    else if (p-2 == q) {
+        // When P and Q differ by 2dB, result is P+0.969 dB
+        p += 2;
+    }
+    else if (p-3 == q) {
+        // When P and Q differ by 3dB, result is P+0.511 dB
+        p += 1;
+    }
+
+    band[index] = p;
+}
+
+//
+// Add the signal level at given channel
 // Channel is in range 1...11
 // RSSI is in range -94...-35 dB
 //
-static void draw_net(int chan, int rssi)
+static void add_channel(int chan, int rssi)
 {
     // Ignore incorrect data.
     if (chan < 1 || chan > 11)
         return;
 
-    int level;
-    if      (rssi <= -90) level = 0;
-    else if (rssi <= -80) level = 1;
-    else if (rssi <= -70) level = 2;
-    else if (rssi <= -60) level = 3;
-    else if (rssi <= -50) level = 4;
-    else if (rssi <= -40) level = 5;
-    else                  level = 6;
-
-    // Draw scale: 8 pixels per 5 MHz
-    //TODO: level
-    int x0 = chan*8 + 8;
-    raise_bar(x0 - 16, 2);
-    raise_bar(x0 - 8,  2);
-    raise_bar(x0,      2);
-    raise_bar(x0 + 8,  2);
-    raise_bar(x0 + 16, 2);
+    // Assume the channel emits the given rssi level at his central band (chan+1),
+    // half this power at neighbour bands (chan, chan+2) - which is rssi-3 dB,
+    // and 1/8 of this power at more distant bands (chan-1, chan+3) - which is rssi-9 dB,
+    add_band(chan-1, rssi-9);
+    add_band(chan,   rssi-3);
+    add_band(chan+1, rssi);
+    add_band(chan+2, rssi-3);
+    add_band(chan+3, rssi-9);
 }
 
 void loop()
@@ -103,7 +158,7 @@ void loop()
     Serial.println("scan done");
     display.clearDisplay();
     display.setCursor(0, 0);
-    reset_bar();
+    reset_bands();
     if (n == 0) {
         display.print("no nets");
         Serial.println("no networks found");
@@ -125,12 +180,13 @@ void loop()
             Serial.print(rssi);
             Serial.print(")");
             Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-            draw_net(chan, rssi);
+            add_channel(chan, rssi);
         }
     }
     Serial.println("");
 
     // Visualize the result.
+    draw_bands();
     display.display();
 
     // Wait a bit before scanning again
