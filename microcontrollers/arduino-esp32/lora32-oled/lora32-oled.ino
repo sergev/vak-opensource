713ef5653ab0abeff1db0acc908148729beef5d2
Serge Vakulenko <serge.vakulenko@gmail.com>
2017-10-09 18:00:29 -0700

    Add makefile for lora32 board.

--- microcontrollers/arduino-esp32/lora32-oled/lora32-oled.ino
+++ microcontrollers/arduino-esp32/lora32-oled/lora32-oled.ino
@@ -5,18 +5,18 @@
   Language: C++
   File: DrawLogo.ino
   ------------------------------------------------------------------------
-  Description: 
-  Drawing an image in the OLED display. The image date was generated using a 
+  Description:
+  Drawing an image in the OLED display. The image date was generated using a
   bitmap generator.
   ------------------------------------------------------------------------
   Please consider buying products from ACROBOTIC to help fund future
   Open-Source projects like this! We'll always put our best effort in every
-  project, and release all our design files and code for you to use. 
+  project, and release all our design files and code for you to use.
   https://acrobotic.com/
   ------------------------------------------------------------------------
   License:
   Released under the MIT license. Please check LICENSE.txt for more
-  information.  All text above must be included in any redistribution. 
+  information.  All text above must be included in any redistribution.
 */
 #include <Wire.h>
 #include <ACROBOTIC_SSD1306.h>
@@ -97,12 +97,12 @@ static unsigned char ACROBOT[] PROGMEM ={
 
 void setup()
 {
-  pinMode(16,OUTPUT);
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 to high  
-  
-  Wire.begin(4,15);  
+  pinMode(16, OUTPUT);
+  digitalWrite(16, LOW);            // set GPIO16 low to reset OLED
+  delay(50);
+  digitalWrite(16, HIGH);           // while OLED is running, must set GPIO16 to high
+
+  Wire.begin(4, 15);
   oled.init();                      // Initialze SSD1306 OLED display
   oled.clearDisplay();              // Clear screen
   oled.drawBitmap(ACROBOT, 1024);   // 1024 pixels for logo
