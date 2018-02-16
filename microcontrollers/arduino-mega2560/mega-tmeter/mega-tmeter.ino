15037ad697f6ff5799f0a6e95a134fe450fb095b
Serge Vakulenko <serge.vakulenko@gmail.com>
2018-02-16 01:24:09 -0800

    Tmeter: remove unused inputs.

--- microcontrollers/arduino-mega2560/mega-tmeter/mega-tmeter.ino
+++ microcontrollers/arduino-mega2560/mega-tmeter/mega-tmeter.ino
@@ -3,39 +3,15 @@
  */
 unsigned gate_output = 0;       // control voltage to the Gate pin of transistor
 unsigned gate_input;            // measured voltage of the Gate pin
-unsigned drain_input;           // measured voltage of the Drain pin
-unsigned drain_current_input;   // measured current of the Drain pin
-unsigned power5_input;          // measured voltage of +5V power source
-unsigned power15_input;         // measured voltage of +15V power source
+unsigned drain_input;           // measured current of the Drain pin
 
 unsigned gate_mv;               // millivolts at the Gate pin
-unsigned drain_mv;              // millivolts at the Drain pin
 unsigned long drain_uamp;       // microamperes at the Drain pin
-unsigned power5_mv;             // +5V power source in millivolts
-unsigned power15_mv;            // +15V power source in millivolts
 
 //
 // Use Timer3, pins 2, 3 and 5 on Mega 2560 board.
 //
 static const int gate_output_pin = 2;
-
-//
-// Calibration coefficients for resistor dividers.
-// Typical resistors are about 2% imprecise.
-// It makes sense to use an external voltmeter to figure out
-// the calibration coefficients for your case.
-//
-#define CALIBRATE_DRAIN     (11.33 / 11.21)     // drain divider
-#define CALIBRATE_POWER5    (5.00  / 5.08)      // +5V divider
-#define CALIBRATE_POWER15   (11.39 / 11.06)     // +15V divider
-
-//
-// Coefficients of resistor dividers.
-//
-#define DRAIN_MULT      (((33 + 10) / 10.0) * CALIBRATE_DRAIN)      // resistors 33k and 10k
-#define POWER5_MULT     (((10 + 10) / 10.0) * CALIBRATE_POWER5)     // resistors 10k and 10k
-#define POWER15_MULT    (((33 + 10) / 10.0) * CALIBRATE_POWER15)    // resistors 33k and 10k
-#define DR_CURRENT_MULT (100000.0 / 220 / 51)                       // resistors 51 and 220
 
 /*
  * Read a character from the serial port.
@@ -112,40 +88,13 @@ void measure_gate()
     gate_mv = gate_input * (5000.0 / 1024 / 3);
 }
 
-/*
- * Measure drain voltage.
- * Repeat until the value is stable for 3 attempts.
- */
-void measure_drain_voltage()
+void measure_drain()
 {
     drain_input = measure(A1);
-    drain_mv = drain_input * ((5000.0 / 1024 / 3) * DRAIN_MULT);
-}
 
-void measure_drain_current()
-{
-    drain_current_input = measure(A4);
-    drain_uamp = drain_current_input * ((5000.0 / 1024 / 3) * DR_CURRENT_MULT);
-}
-
-/*
- * Measure +5V voltage.
- * Repeat until the value is stable for 3 attempts.
- */
-void measure_power5()
-{
-    power5_input = measure(A2);
-    power5_mv = power5_input * ((5000.0 / 1024 / 3) * POWER5_MULT);
-}
-
-/*
- * Measure +15V voltage.
- * Repeat until the value is stable for 3 attempts.
- */
-void measure_power15()
-{
-    power15_input = measure(A3);
-    power15_mv = power15_input * ((5000.0 / 1024 / 3) * POWER15_MULT);
+    // ZXCT1009 current sense monitor is connected via
+    // 51 Ohm resistor as a drain shunt, and 220 Ohm resistor as a load.
+    drain_uamp = drain_input * ((5000.0 / 1024 / 3) * (100000.0 / 220 / 51));
 }
 
 /*
@@ -155,10 +104,7 @@ void show_state()
 {
     // Update input values.
     measure_gate();
-    measure_drain_voltage();
-    measure_drain_current();
-    measure_power5();
-    measure_power15();
+    measure_drain();
 
     // Display all values.
     Serial.write("\r\n  Gate output: ");
@@ -170,37 +116,11 @@ void show_state()
     Serial.print(gate_input);
     Serial.write(")");
 
-    Serial.write("\r\nDrain voltage: ");
-    if (power15_mv > 5000) {
-        print_mv(drain_mv);
-        Serial.write(" (");
-        Serial.print(drain_input);
-        Serial.write(")");
-    } else {
-        Serial.write("No power!");
-    }
-
     Serial.write("\r\nDrain current: ");
     print_uamp(drain_uamp);
     Serial.write(" (");
-    Serial.print(drain_current_input);
+    Serial.print(drain_input);
     Serial.write(")");
-
-    Serial.write("\r\n    Power +5V: ");
-    print_mv(power5_mv);
-    Serial.write(" (");
-    Serial.print(power5_input);
-    Serial.write(")");
-
-    Serial.write("\r\n   Power +15V: ");
-    if (power15_mv > 5000) {
-        print_mv(power15_mv);
-        Serial.write(" (");
-        Serial.print(power15_input);
-        Serial.write(")");
-    } else {
-        Serial.write("Not connected!");
-    }
 
     Serial.write("\r\n");
 }
@@ -220,11 +140,11 @@ void measure_jfet()
     Serial.write("\r\nMeasuring N-JFET:\r\n\r\n");
     Serial.write(" Vg, V   Id, mA\r\n");
     Serial.write("---------------\r\n");
-    //for (i=255; i>=0; i--) {
-    for (i=0; i<256; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    for (i=255; i>=0; i--) {
         set_gate(i);
         measure_gate();
-        measure_drain_current();
+        measure_drain();
 
         // Print results.
         Serial.write(" ");
@@ -232,15 +152,13 @@ void measure_jfet()
         Serial.write("  ");
         Serial.print(drain_uamp / 1000.0, 3);
         Serial.write("\r\n");
-#if 0
-        // Stop when last three values are same.
-        if (v1 == v2 && v2 == drain_current_input)
+
+        // Stop on zero.
+        if (drain_input == 0)
             break;
-        v1 = v2;
-        v2 = drain_current_input;
-#endif
     }
     set_gate(0);
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.write("Done\r\n");
 }
 
@@ -306,17 +224,12 @@ void setup()
     // Configure pins.
     // D2 - gate output
     // A0 - gate input
-    // A1 - drain voltage input with divisor 1:4.3
-    // A2 - +5V input with divisor 1:2
-    // A3 - +15V input with divisor 1:4.3
-    // A4 - drain current input via ZXCT1009 current sense monitor
+    // A1 - drain current input via ZXCT1009 current sense monitor
     //
+    pinMode(LED_BUILTIN, OUTPUT);
     pinMode(gate_output_pin, OUTPUT);
     pinMode(A0, INPUT);
     pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
-    pinMode(A3, INPUT);
-    pinMode(A4, INPUT);
 
     // Configure timer 3 prescale factor as 1:1.
     // Set bits CS32,CS31,CS30 to 0b001.
