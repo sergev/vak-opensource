f937b4595835a1ca2d841514459e2872236e4481
Serge Vakulenko <serge.vakulenko@gmail.com>
2019-02-03 15:52:41 -0800

    ATtiny demo: enable pull-up resistors.

--- microcontrollers/arduino-attiny/attiny-blink/attiny-blink.cpp
+++ microcontrollers/arduino-attiny/attiny-blink/attiny-blink.cpp
@@ -33,7 +33,9 @@ int main()
 {
     // Configure LED1 and LED2 pins as output
     DDRB = B_LED1 | B_LED2;
-    PORTB = 0;
+
+    // Enable pull-up resistors on key1 and key2 pins.
+    PORTB = B_KEY1 | B_KEY2;
 
 #if F_CPU > 1200000
     // Disable 1:8 clock divisor: raise from 1.2Mhz tp 9.6MHz.
@@ -42,20 +44,32 @@ int main()
 #endif
 
     while (1) {
-        PORTB |= B_LED1;
-        PORTB &= ~B_LED2;
-        mdelay(250);
+        PORTB |= B_LED1;    // LED1 on
+        PORTB &= ~B_LED2;   // LED2 off
+        mdelay(100);
 
-        // Buttons don't work: pull-up resistors missing on this board.
-        //while (!(PINB & B_KEY2))
-        //    continue;
+        // Buttons work when pull-up resistors are enabled.
+        while (!(PINB & B_KEY2)) {
+            // Blink LED1 fast, while key2 is depressed.
+            PORTB |= B_LED2;
+            mdelay(50);
+            PORTB &= ~B_LED2;
+            mdelay(50);
+            continue;
+        }
 
-        PORTB &= ~B_LED1;
-        PORTB |= B_LED2;
-        mdelay(250);
+        PORTB &= ~B_LED1;   // LED1 on
+        PORTB |= B_LED2;    // LED2 off
+        mdelay(100);
 
-        //while (!(PINB & B_KEY1))
-        //    continue;
+        while (!(PINB & B_KEY1)) {
+            // Blink LED2 fast, while key1 is depressed.
+            PORTB |= B_LED1;
+            mdelay(50);
+            PORTB &= ~B_LED1;
+            mdelay(50);
+            continue;
+        }
     }
 
     return 0;
