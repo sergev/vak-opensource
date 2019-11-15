9844ab94448f2912c5337823571ea685316b5fcf
Serge Vakulenko <serge.vakulenko@gmail.com>
2019-11-15 12:20:05 -0800

    Update revlist.

--- languages/c-language/revlist.c
+++ languages/c-language/revlist.c
@@ -12,32 +12,43 @@ struct list_t {
 };
 
 //
-// Revert list `p' and append `next' to it.
+// Revert list `p' and append `append' to it.
+// Recursive algorithm.
 //
-list_t *revert(list_t *p, list_t *next)
+list_t *revert_recursive(list_t *p, list_t *append)
 {
-#if 0
-    // Recursive
     list_t *tail = p->next;
 
-    p->next = next;
+    p->next = append;
     if (!tail)
         return p;
-    return revert(tail, p);
-#else
-    // Converted to a loop
+
+    return revert_recursive(tail, p);
+}
+
+//
+// Revert list `p'.
+// Iterative algorithm.
+//
+list_t *revert(list_t *p)
+{
+    list_t *append = 0;
+
     for (;;) {
         list_t *tail = p->next;
 
-        p->next = next;
+        p->next = append;
         if (!tail)
             return p;
-        next = p;
+
+        append = p;
         p = tail;
     }
-#endif
 }
 
+//
+// Print list `p'.
+//
 void print_list(list_t *p)
 {
     while (p) {
@@ -55,8 +66,12 @@ int main()
     list_t a = { &b, 'A' };
     print_list(&a);
 
-    // Revert the list.
-    list_t *r = revert(&a, 0);
+    // Revert with recursive algorithm.
+    list_t *r = revert_recursive(&a, 0);
+    print_list(r);
+
+    // Revert back with iterative algorithm.
+    r = revert(r);
     print_list(r);
     return 0;
 }
