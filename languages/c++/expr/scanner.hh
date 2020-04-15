f02579f6af8e5d0464cacacb3ac914651a5cae30
Serge Vakulenko <serge.vakulenko@gmail.com>
2020-04-14 22:37:20 -0700

    Add gtest demo.

--- languages/c++/expr/scanner.hh
+++ languages/c++/expr/scanner.hh
@@ -7,9 +7,6 @@ namespace Demo {
 
 class Scanner : public yyFlexLexer {
 public:
-    Scanner() {}
-    virtual ~Scanner() {}
-
     Grammar::token::yytokentype get_next_token(Grammar::semantic_type *const lval,
                                                Grammar::location_type *loc);
 };
---
+++ utilities/gtest-demo/README.txt
@@ -0,0 +1,11 @@
+A skeleton for a C++ test, based on Google Test.
+
+For details, see: https://github.com/google/googletest/blob/master/googletest/README.md
+
+How to build and run:
+
+    mkdir build
+    cd build
+    cmake ..
+    make
+    ./test
