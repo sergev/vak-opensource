ce0a872240e447b0a2d068807febc4276344a02b
Serge <serge.vakulenko@gmail.com>
2020-11-14 00:41:26 -0800

    Add demos for C# and F#.

---
+++ languages/f#/mstest/Tests.fs
@@ -0,0 +1,11 @@
+namespace mstest
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
