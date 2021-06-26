c3ae608d61669d483c8062a41e4e2bc27d71fe92
Serge Vakulenko <serge.vakulenko@gmail.com>
2021-06-25 22:08:44 -0700

    Update hello world for esp-idf.

--- microcontrollers/esp-idf/hello_world/main/hello_world_main.c
+++ microcontrollers/esp-idf/hello_world/main/hello_world_main.c
@@ -13,14 +13,6 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-#ifdef CONFIG_IDF_TARGET_ESP32
-#define CHIP_NAME "ESP32"
-#endif
-
-#ifdef CONFIG_IDF_TARGET_ESP32S2BETA
-#define CHIP_NAME "ESP32-S2 Beta"
-#endif
-
 void app_main(void)
 {
     printf("Hello world!\n");
@@ -28,8 +20,8 @@ void app_main(void)
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CHIP_NAME,
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+            CONFIG_IDF_TARGET,
             chip_info.cores,
             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
@@ -38,6 +30,8 @@ void app_main(void)
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
