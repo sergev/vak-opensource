d51bb31583c6cedefcce7969d23198a76ccd4198
Serge Vakulenko <serge.vakulenko@sima.ai>
2023-03-16 18:27:11 -0700

    Add Zig examples.

---
+++ languages/zig/zig-curl-test.zig
@@ -0,0 +1,51 @@
+// compile with `zig build-exe zig-curl-test.zig --library curl --library c $(pkg-config --cflags libcurl)`
+const std = @import("std");
+const cURL = @cImport({
+    @cInclude("curl/curl.h");
+});
+
+pub fn main() !void {
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena_state.deinit();
+
+    const allocator = arena_state.allocator();
+
+    // global curl init, or fail
+    if (cURL.curl_global_init(cURL.CURL_GLOBAL_ALL) != cURL.CURLE_OK)
+        return error.CURLGlobalInitFailed;
+    defer cURL.curl_global_cleanup();
+
+    // curl easy handle init, or fail
+    const handle = cURL.curl_easy_init() orelse return error.CURLHandleInitFailed;
+    defer cURL.curl_easy_cleanup(handle);
+
+    var response_buffer = std.ArrayList(u8).init(allocator);
+
+    // superfluous when using an arena allocator, but
+    // important if the allocator implementation changes
+    defer response_buffer.deinit();
+
+    // setup curl options
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_URL, "https://ziglang.org") != cURL.CURLE_OK)
+        return error.CouldNotSetURL;
+
+    // set write function callbacks
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEFUNCTION, writeToArrayListCallback) != cURL.CURLE_OK)
+        return error.CouldNotSetWriteCallback;
+    if (cURL.curl_easy_setopt(handle, cURL.CURLOPT_WRITEDATA, &response_buffer) != cURL.CURLE_OK)
+        return error.CouldNotSetWriteCallback;
+
+    // perform
+    if (cURL.curl_easy_perform(handle) != cURL.CURLE_OK)
+        return error.FailedToPerformRequest;
+
+    std.log.info("Got response of {d} bytes", .{response_buffer.items.len});
+    std.debug.print("{s}\n", .{response_buffer.items});
+}
+
+fn writeToArrayListCallback(data: *anyopaque, size: c_uint, nmemb: c_uint, user_data: *anyopaque) callconv(.C) c_uint {
+    var buffer = @intToPtr(*std.ArrayList(u8), @ptrToInt(user_data));
+    var typed_data = @intToPtr([*]u8, @ptrToInt(data));
+    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
+    return nmemb * size;
+}
