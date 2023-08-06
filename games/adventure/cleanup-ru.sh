8e0eb1bef8432a376e122b6835448044ae163e2e
Serge Vakulenko <serge.vakulenko@gmail.com>
2023-08-05 22:01:39 -0700

    Add walktrough for Colossal Cave Adventure in Russian.

--- games/adventure/cleanup-ru.sh
+++ games/adventure/cleanup-ru.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-chmod -x game.log
+chmod -x game-ru.log
 
 # Remove \r
 sed -i~ '
 s///g
-' game.log
+' game-ru.log
 
 # Remove extra newlines
 sed -i~ '
 /^>/{N;N;s/\n\n/\
 /;}
-' game.log
+' game-ru.log
