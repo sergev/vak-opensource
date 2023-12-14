d45be3cc626c40b793b6ce96698b731a865194bc
Serge Vakulenko <serge.vakulenko@gmail.com>
2023-12-14 01:00:09 -0800

    Update the 'blink' demo for WS2812 LED on RP2040 board.

--- microcontrollers/raspberry-pico/blink/blink.c
+++ microcontrollers/raspberry-pico/blink/blink.c
@@ -23,10 +23,19 @@ static inline unsigned color_rgb(uint8_t r, uint8_t g, uint8_t b)
     return (r << 16) | (g << 8) | b;
 }
 
+void blink(unsigned color, unsigned msec)
+{
+    led_write(color);
+    sleep_ms(msec);
+    led_write(0);
+    sleep_ms(msec);
+}
+
 int main()
 {
     // WS2812 LED on rp2040-zero connected to GP16 pin.
-    const unsigned WS2812_PIN = 16;
+//  const unsigned WS2812_PIN = 16; // Waveshare RP2040-Zero board
+    const unsigned WS2812_PIN = 23; // vcc-gnd YD-RP2040 board
     const bool IS_RGBW = false;
     const unsigned SM = 0;
     const float FREQ = 800000;
@@ -36,21 +45,18 @@ int main()
 
     for (;;) {
         // red
-        led_write(color_rgb(0xff, 0, 0));
-        sleep_ms(250);
-        led_write(0);
-        sleep_ms(250);
+        blink(color_rgb(0xff, 0, 0), 250);
+        blink(color_rgb(0xff, 0, 0), 250);
 
         // green
-        led_write(color_rgb(0, 0xff, 0));
-        sleep_ms(250);
-        led_write(0);
-        sleep_ms(250);
+        blink(color_rgb(0, 0xff, 0), 250);
+        blink(color_rgb(0, 0xff, 0), 250);
+        blink(color_rgb(0, 0xff, 0), 250);
 
         // blue
-        led_write(color_rgb(0, 0, 0xff));
-        sleep_ms(250);
-        led_write(0);
-        sleep_ms(250);
+        blink(color_rgb(0, 0, 0xff), 250);
+        blink(color_rgb(0, 0, 0xff), 250);
+        blink(color_rgb(0, 0, 0xff), 250);
+        blink(color_rgb(0, 0, 0xff), 250);
     }
 }
