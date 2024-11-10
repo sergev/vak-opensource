998544ccb331519d0b6140b3d17ac9dce36bc717
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-11-09 21:29:38 -0800

    Simplify cubic root.

--- languages/assembler/riscv/cubic_root.s
+++ languages/assembler/riscv/cubic_root.s
@@ -1,10 +1,10 @@
 # double cubic_root(double n)
 # {
-#   double x = 1.0; # Initial guess
+#   double x = 1.0;
 #   for (;;) {
-#       double nx = ((n / x / x) + x + x) / 3.0;
+#       double nx = (n/x/x + x + x) / 3.0;
 #       if (nx == x) {
-#           return nx;
+#           return x;
 #       }
 #       x = nx;
 #   }
@@ -15,22 +15,20 @@ cubic_root:
         fmv.d   fa3, fa0            # n in register fa3
         fld     fa2, three, a5      # constant 3.0
         fld     fa0, one, a5        # x = 1.0, initial guess
-1:
+loop:
         fdiv.d  fa5, fa3, fa0       # n / x
-        fmv.d   fa4, fa0            # old x in registe fa4
+        fmv.d   fa4, fa0            # old x in register fa4
         fdiv.d  fa5, fa5, fa0       # n / x / x
         fadd.d  fa5, fa5, fa0       # + x
         fadd.d  fa5, fa5, fa0       # + x
         fdiv.d  fa0, fa5, fa2       # divide by 3.0
         feq.d   a5, fa4, fa0        # nx == x?
-        beq     a5, zero, 1b        # otherwise to next iteration
+        beqz    a5, loop            # otherwise to next iteration
         ret
 
-        .align  3
-one:    .quad   0x3ff0000000000000  # 1.0
-three:  .quad   0x4008000000000000  # 3.0
+one:    .double 1.0
+three:  .double 3.0
 
-        .text
         .globl  main
         .type   main, @function
 main:
@@ -45,8 +43,7 @@ main:
         call    __printf_chk
 
         li      a0, 0               # exit status
-        call    _exit
+        call    _exit               # terminate
 
-        .align  3
-input:  .quad   0x4000000000000000  # 2.0
+input:  .double 2.0
 format: .string "cubic root of %.15g is %.15g\n"
