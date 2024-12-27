2b8f90cb1a3f50f3b45bd4e20ec6a93b4c3ebd0b
Serge <serge.vakulenko@gmail.com>
2024-12-26 23:56:24 -0800

    Move game-of-go to a separate repo.

--- languages/python/game-of-go/README.txt
+++
@@ -1,4 +0,0 @@
-Examples from book "Deep Learning and the Game of Go".
-https://github.com/maxpumperla/deep_learning_and_the_game_of_go/tree/master
-
- * generate_mcts_games.py  - Listing 6.7 from page 123
--- languages/python/game-of-go/dlgo/encoders/__init__.py
+++
@@ -1,6 +0,0 @@
-from dlgo.encoders.base import *
-#from dlgo.encoders.alphago import *
-#from dlgo.encoders.betago import *
-from dlgo.encoders.oneplane import *
-#from dlgo.encoders.sevenplane import *
-#from dlgo.encoders.simple import *
