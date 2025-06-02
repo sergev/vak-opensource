929883e5fd93d8ebe7163659619204d992923dd2
Serge <serge.vakulenko@gmail.com>
2025-06-02 14:25:05 -0700

    Enhance goecho.go.

--- languages/go/goecho.go
+++ languages/go/goecho.go
@@ -1,28 +1,71 @@
 package main
 
 import (
-	"os"
-	"flag" // парсер параметров командной строки
-)
-
-var omitNewline = flag.Bool("n", false, "не печатать последнюю линию")
-
-const (
-	Space   = " "
-	Newline = "\n"
+    "flag"
+    "fmt"
+    "strings"
 )
 
 func main() {
-	flag.Parse() // Сканирование списка аргументов и установка флагов
-	var s string = ""
-	for ы := 0; ы < flag.NArg(); ы++ {
-		if ы > 0 {
-			s += Space
-		}
-		s += flag.Arg(ы)
-	}
-	if !*omitNewline {
-		s += Newline
-	}
-	os.Stdout.WriteString(s)
+    // Define command-line flags
+    var noNewline bool
+    var enableEscapes bool
+    flag.BoolVar(&noNewline, "n", false, "do not output the trailing newline")
+    flag.BoolVar(&enableEscapes, "e", false, "enable interpretation of backslash escapes")
+    flag.Parse()
+
+    // Get the arguments after flags
+    args := flag.Args()
+
+    // Process the input string
+    output := strings.Join(args, " ")
+
+    if enableEscapes {
+        output = interpretEscapes(output)
+    }
+
+    // Print the output
+    fmt.Print(output)
+
+    // Add newline unless -n is specified
+    if !noNewline {
+        fmt.Println()
+    }
+}
+
+// interpretEscapes processes backslash escape sequences
+func interpretEscapes(input string) string {
+    var result strings.Builder
+    i := 0
+    for i < len(input) {
+        if input[i] == '\\' && i+1 < len(input) {
+            i++
+            switch input[i] {
+            case 'a':
+                result.WriteByte('\a') // Alert (bell)
+            case 'b':
+                result.WriteByte('\b') // Backspace
+            case 'f':
+                result.WriteByte('\f') // Form feed
+            case 'n':
+                result.WriteByte('\n') // Newline
+            case 'r':
+                result.WriteByte('\r') // Carriage return
+            case 't':
+                result.WriteByte('\t') // Tab
+            case 'v':
+                result.WriteByte('\v') // Vertical tab
+            case '\\':
+                result.WriteByte('\\') // Backslash
+            default:
+                // If not a recognized escape, keep the backslash and character
+                result.WriteByte('\\')
+                result.WriteByte(input[i])
+            }
+        } else {
+            result.WriteByte(input[i])
+        }
+        i++
+    }
+    return result.String()
 }
